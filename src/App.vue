59e79b394917f2d4c39d6747ef8d9bcf554b9cd8
Woody134 <mine-1234@qq.com>
2017-11-08 17:52:00 +0800

    learn components & router

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <v-header></v-header>
+    <div class="tab">
+      <router-link to="/goods" class="tab-item">商品</router-link>
+      <router-link to="/ratings" class="tab-item">评论</router-link>
+      <router-link to="/seller" class="tab-item">商家</router-link>       
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import header from './components/header/header.vue';
+
+  export default {
+    components: {
+      'v-header': header
+    }
+  };
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" rel="stylesheet/stylus">
+  .tab
+    display:flex
+    width: 100%
+    height: 40px
+    line-height: 40px
+    .tab-item
+      flex: 1
+      text-align: center
 </style>
